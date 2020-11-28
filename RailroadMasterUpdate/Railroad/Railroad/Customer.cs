41355a261da5caecebf4d92d68a0301fbb67a39d
Chelsie87 <71669481+Chelsie87@users.noreply.github.com>
2020-11-28 12:16:34 -0600

    Add files via upload
    Bug fix merge:
    -Send button can be clicked first without crashing
    -ID textboxes only allow numerical input
    -Tab order is ideal (name --> ID --> railcar --> priority --> pickup --> Add --> Send --> Clear)
    -Added a help drop down menu. Clicking on the step will focus on that element. Checkboxes do not focus well however,

---
+++ RailroadMasterUpdate/Railroad/Railroad/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Railroad
+{
+    class Customer
+    {
+        //customer class
+        //records all information about a customer
+
+        //request delivery/pickup functions??
+
+        private readonly string customer_name;
+        private readonly int customer_ID;
+
+        public Customer()
+        {
+
+        }
+        public string Customer_name { get; set; }
+        public int Customer_ID { get; set; }
+        public override string ToString() => $"({Customer_ID}: {Customer_name})";
+    }
+}
